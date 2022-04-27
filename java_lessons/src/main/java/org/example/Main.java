4899b3fe9331e0ee554af82634316eb5c724682c
ReyZo09 <87603087+ReyZo09@users.noreply.github.com>
2022-04-27 14:28:47 +0400

    -отредактирован код

--- java_lessons/src/main/java/org/example/Main.java
+++ java_lessons/src/main/java/org/example/Main.java
@@ -1,17 +1,9 @@
 package org.example;
-
-
-
 public class Main {
-
     public static void main(String[] args) {
         Point a = new Point(11, 13);
         Point b = new Point(18.34, 36.11);
         System.out.println(Point.range(a, b));
 
     }
-
-
-
-
 }
